98c63b285344b0068390c01aeac1e87fbfa36b46
gui2one <guillaume.rouault001@orange.fr>
2015-08-20 11:04:18 +0200

    hello

--- misc/launch_mplay.py
+++ misc/launch_mplay.py
@@ -3,9 +3,10 @@ import time
 # import win32gui
 
 
-
+path = r"F:\HOUDINI_14_playground\render\wood_way\03\wood_way_with_tree_\*.exr"
+formatedPath = path.replace("\\","\\\\")
 while True:
-	process = subprocess.Popen("C:\\Program Files\\Side Effects Software\\Houdini 14.0.395\\bin\\mplay.exe -p F:\\HOUDINI_14_playground\\render\\tortured_tree\\01\\* " )
+	process = subprocess.Popen("C:/Program Files/Side Effects Software/Houdini 14.0.395/bin/mplay.exe -p -z 200 F:/HOUDINI_14_playground/render/wood_way/03/wood_way_with_tree_*.exr") 
 
 	# handle = win32gui.FindWindow(None, u"MPlay*")
 	# time.sleep(10)
