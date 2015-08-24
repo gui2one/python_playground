8f8e1920a237571ad4fbbf9b91feb569bf92c611
gui2one <guillaume.rouault001@orange.fr>
2015-08-24 09:51:04 +0200

    launch_mplay

--- misc/console_stdout.py
+++ misc/console_stdout.py
@@ -5,9 +5,21 @@ def restart_line():
     sys.stdout.write('\r')
     sys.stdout.flush()
 
-sys.stdout.write('some data')
-sys.stdout.flush()
-time.sleep(2) # wait 2 seconds...
-restart_line()
-sys.stdout.write('other different data')
-sys.stdout.flush()
+def drawBar(inc):
+	string = ""
+	for i in range(0,inc):
+		string += "="
+
+	return string
+
+for i in range(0,51):
+	# sys.stdout.write('some data')
+	sys.stdout.flush()
+	time.sleep(0.1) # wait 2 seconds...
+	restart_line()
+	outStr = 'other different data '+ str(i) 
+	sys.stdout.write(str(i) + "-->" + drawBar(i))
+	sys.stdout.flush()
+
+
+print '\n'
--- misc/launch_mplay.py
+++ misc/launch_mplay.py
@@ -6,7 +6,7 @@ import time
 path = r"F:\HOUDINI_14_playground\render\wood_way\03\wood_way_with_tree_\*.exr"
 formatedPath = path.replace("\\","\\\\")
 while True:
-	process = subprocess.Popen("C:/Program Files/Side Effects Software/Houdini 14.0.395/bin/mplay.exe -p -z 200 F:/HOUDINI_14_playground/render/wood_way/03/wood_way_with_tree_*.exr") 
+	process = subprocess.Popen("C:/Program Files/Side Effects Software/Houdini 14.0.395/bin/mplay.exe -p -z 100 -r 15 F:/HOUDINI_14_playground/render/light_bulbs/volume/volume.*.exr") 
 
 	# handle = win32gui.FindWindow(None, u"MPlay*")
 	# time.sleep(10)
@@ -14,5 +14,8 @@ while True:
 
 
 	# print handle
-	time.sleep(300)
-	process.kill()
+	time.sleep(60*5)
+	try:
+		process.kill()
+	except:
+		print "no process to kill -- continuing"
