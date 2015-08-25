d71e028ca6a322e0685b8afba50570331b56565c
gui2one <guillaume.rouault001@orange.fr>
2015-08-25 11:14:57 +0200

    hey

--- misc/launch_mplay.py
+++ misc/launch_mplay.py
@@ -6,7 +6,7 @@ import time
 path = r"F:\HOUDINI_14_playground\render\wood_way\03\wood_way_with_tree_\*.exr"
 formatedPath = path.replace("\\","\\\\")
 while True:
-	process = subprocess.Popen("C:/Program Files/Side Effects Software/Houdini 14.0.395/bin/mplay.exe -p -z 100 -r 15 F:/HOUDINI_14_playground/render/light_bulbs/volume/volume.*.exr") 
+	process = subprocess.Popen("C:/Program Files/Side Effects Software/Houdini 14.0.395/bin/mplay.exe -p -z 100 -r 30 F:/HOUDINI_14_playground/render/anim_typos/02/anim_typos_02_*.exr") 
 
 	# handle = win32gui.FindWindow(None, u"MPlay*")
 	# time.sleep(10)
@@ -14,7 +14,7 @@ while True:
 
 
 	# print handle
-	time.sleep(60*5)
+	time.sleep(60*10)
 	try:
 		process.kill()
 	except:
