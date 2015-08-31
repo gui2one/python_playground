822bc455be72ba72287c92a0dc81381a5d012549
gui2one <guillaume.rouault001@orange.fr>
2015-08-31 09:51:05 +0200

    commit script

--- misc/launch_mplay.py
+++ misc/launch_mplay.py
@@ -1,12 +1,19 @@
 import subprocess
 import time
-# import win32gui
+import glob
 
 
-path = r"F:\HOUDINI_14_playground\render\wood_way\03\wood_way_with_tree_\*.exr"
+path = "F:/HOUDINI_14_playground/render/i_love_this_dance/04/*"
+
+
+
+
+#path = r"F:\HOUDINI_14_playground\render\wood_way\03\wood_way_with_tree_\*.exr"
 formatedPath = path.replace("\\","\\\\")
+
+
 while True:
-	process = subprocess.Popen("C:/Program Files/Side Effects Software/Houdini 14.0.395/bin/mplay.exe -p -z 100 -r 30 F:/HOUDINI_14_playground/render/stade_rennais/anim_logos/04/2_logos/2_logos.*.exr F:/HOUDINI_14_playground/render/stade_rennais/anim_logos/04/light_lines/light_lines.*.exr") 
+	process = subprocess.Popen("C:/Program Files/Side Effects Software/Houdini 14.0.395/bin/mplay.exe -p -z 100 -r 30 F:/HOUDINI_14_playground/render/i_love_this_dance/04/_anim_04_*.exr") 
 
 	# handle = win32gui.FindWindow(None, u"MPlay*")
 	# time.sleep(10)
@@ -14,7 +21,28 @@ while True:
 
 
 	# print handle
-	time.sleep(60*10)
+	obj  = glob.glob(path)
+	num = len(obj)
+	oldNum = num
+	
+
+	bBreak = False
+	while not bBreak :
+
+		newNum = len(glob.glob(path))
+		print newNum
+		if newNum > oldNum :
+			print "new file detected"
+			bBreak = True
+
+		else:
+			print "sleep 5 seconds"
+			time.sleep(60*3)
+
+	
+	print num
+
+	#time.sleep(60*10)
 	try:
 		process.kill()
 	except:
