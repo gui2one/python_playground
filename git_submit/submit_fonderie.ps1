b30b47ab5a5124e97c7873f0ca5a3175959f5444
gui2one <guillaume.rouault001@orange.fr>
2016-02-10 11:53:18 +0100

    commit script Fonderie

---
+++ git_submit/submit_fonderie.ps1
@@ -0,0 +1,9 @@
+Copy-Item 'C:\Program Files\Blender Foundation\Blender\2.76\scripts\startup\HOUDINI_SCENE_panel.py' -Destination F:\PYTHON_playground\blender\HOUDINI_scene_loader
+
+Cd F:
+Cd PYTHON_playground
+git add -A
+git commit -m 'commit script Fonderie'
+
+git push
+pause
