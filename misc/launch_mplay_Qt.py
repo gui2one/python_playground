4a5099c308d1951f1cb8b25f3570f4c090c80779
gui2one <guillaume.rouault001@orange.fr>
2016-05-10 10:50:22 +0200

    commit script Fonderie

--- misc/launch_mplay_Qt.py
+++ misc/launch_mplay_Qt.py
@@ -4,77 +4,79 @@ import subprocess
 import time
 import sys 
 import threading 
+import re
+import os
 
-class My_Thread(threading.Thread):
+# class My_Thread(threading.Thread):
 
-    def __init__(self):
-		threading.Thread.__init__(self)
-		self.process = None
-		self.MY_GLOBAL = False
-		self.CANCELED = False
-		self.MPLAY_process = None        
+#     def __init__(self):
+# 		threading.Thread.__init__(self)
+# 		self.process = None
+# 		self.MY_GLOBAL = False
+# 		self.CANCELED = False
+# 		self.MPLAY_process = None        
 
-    def run(self):
+#     def run(self):
 
-		print "Starting " + self.name
-		self.CANCELED = False
-		bBreak = False
-		path = "F:/BLENDER_playground/*.png"
-		# F:\HOUDINI_15_playground\render\fur_sea
-		obj = glob.glob(path)
-		newNum = len(obj)
-
-
-		while True:
-			'''command options *
-				-T : always on top
-
-			'''
-			self.MPLAY_process = subprocess.Popen("C:/Program Files/Side Effects Software/Houdini 15.0.313/bin/mplay.exe -minimal -p -z 100 -f 1 "+str(newNum)+" 1 -r 25 F:/BLENDER_playground/*.png",shell=False,stdin=subprocess.PIPE,stdout=subprocess.PIPE) 
+# 		print "Starting " + self.name
+# 		self.CANCELED = False
+# 		bBreak = False
+# 		path = "F:/BLENDER_playground/*.png"
+# 		# F:\HOUDINI_15_playground\render\fur_sea
+# 		obj = glob.glob(path)
+# 		newNum = len(obj)
 
 
-			# print handle
-			obj  = glob.glob(path)
-			num = len(obj)
-			oldNum = num
+# 		while True:
+# 			'''command options *
+# 				-T : always on top
+
+# 			'''
+# 			self.MPLAY_process = subprocess.Popen("C:/Program Files/Side Effects Software/Houdini 15.0.313/bin/mplay.exe -minimal -p -z 100 -f 1 "+str(newNum)+" 1 -r 25 F:/BLENDER_playground/*.png",shell=False,stdin=subprocess.PIPE,stdout=subprocess.PIPE) 
+
+
+# 			# print handle
+# 			obj  = glob.glob(path)
+# 			num = len(obj)
+# 			oldNum = num
 			
 
-			bBreak = self.CANCELED
-			while not bBreak :
+# 			bBreak = self.CANCELED
+# 			while not bBreak :
 
-				bBreak = self.MY_GLOBAL
-				newNum = len(glob.glob(path))
-				print newNum
-				if newNum > oldNum :
-					print "new file detected"
-					bBreak = True
+# 				bBreak = self.MY_GLOBAL
+# 				newNum = len(glob.glob(path))
+# 				print newNum
+# 				if newNum > oldNum :
+# 					print "new file detected"
+# 					bBreak = True
 
-				else:
-					n = 0.01
-					print ("sleep %s minutes" % (n))
-					print ('break -->', bBreak)
-					# print process.stdin
-					time.sleep(60*n)
+# 				else:
+# 					n = 0.01
+# 					print ("sleep %s minutes" % (n))
+# 					print ('break -->', bBreak)
+# 					# print process.stdin
+# 					time.sleep(60*n)
 
 			
-			print num
+# 			print num
 
-			#time.sleep(60*10)
-			try:
-				self.MPLAY_process.kill()
-				if self.CANCELED:
-					print "exit while loop"
-					bBreak = False
-					break
-			except:
-				print "no process to kill -- continuing"
-		print "Exiting " + self.name
+# 			#time.sleep(60*10)
+# 			try:
+# 				self.MPLAY_process.kill()
+# 				if self.CANCELED:
+# 					print "exit while loop"
+# 					bBreak = False
+# 					break
+# 			except:
+# 				print "no process to kill -- continuing"
+# 		print "Exiting " + self.name
 
-    def stop(self):
-        print "Trying to stop thread "
-        if self.process is not None:
-            self.process.terminate()
-            self.process = None
+#     def stop(self):
+#         print "Trying to stop thread "
+#         if self.process is not None:
+#             self.process.terminate()
+#             self.process = None
 
 
 class myClass(QtGui.QMainWindow):
@@ -85,10 +87,10 @@ class myClass(QtGui.QMainWindow):
 
 		self.labelSrc = QtGui.QLabel(self)
 		self.labelSrc.setText(self.SRCPATH)
-		self.labelSrc.resize(300,30)
+		self.labelSrc.resize(1000,30)
 		self.labelSrc.move(10,10)
 
-		self.choosePathButton = QtGui.QPushButton('Choose forlder', self)	
+		self.choosePathButton = QtGui.QPushButton('Choose Sequence', self)	
 		self.choosePathButton.clicked.connect(lambda :self.choosePath())
 		self.choosePathButton.move(10,40)
 		self.choosePathButton.setFixedHeight(20)
@@ -126,30 +128,27 @@ class myClass(QtGui.QMainWindow):
 		# print listSrc
 		# print "browse function"
 		dialog = QtGui.QFileDialog(self)
-		dialog.setFileMode(QtGui.QFileDialog.DirectoryOnly)
-		dialog.setOption(QtGui.QFileDialog.ShowDirsOnly)   
+		#dialog.setFileMode(QtGui.QFileDialog.DirectoryOnly)
+		#dialog.setOption(QtGui.QFileDialog.ShowDirsOnly)   
 		if dialog.exec_() :
 
 			self.SRCPATH = dialog.selectedFiles()[0]
 			# srcFolderPath = SRCPATH
 
 			self.labelSrc.setText(self.SRCPATH)
+			p = self.SRCPATH
+			
+			digits = re.findall('\d+', p)
+			digitsString = str(digits[-1:][0])
+			basename , extension = os.path.splitext(p)
+			print 'basename :', basename.strip(digitsString), 'extension :',  extension
 
-			# os.chdir(srcFolderPath) # sets the working directory
-			# fileNames = glob("*")  
-			# # print fileNames           
-			# model = QtGui.QStandardItemModel(self.listSrc)
-			# for afile in fileNames :
-			# 	# print str(afile.endswith("hdalc"))
-			# 	if os.path.isfile(afile) :
-			# 	    if afile.endswith("otl") | afile.endswith("hdalc") :
-			# 	      item = QtGui.QStandardItem(afile)
-			# 	      # item.setCheckable(True)
-			# 	      item.setToolTip('self')
+			mPlayNameString = basename.strip(digitsString)+'*'+extension
 
-			# 	      model.appendRow(item)
+			self.SRCPATH = mPlayNameString
 
-			# 	self.listSrc.setModel(model)
+			return [mPlayNameString,55]
+
 
 
 
@@ -162,11 +161,16 @@ class myClass(QtGui.QMainWindow):
 		print(process)
 
 		try:
-			self.MPLAY_process.kill()
+			
 			self.CANCELED = True
 	
 		except:
-			print "ou on est la ?"		
+			print "ou on est la ?"	
+
+		try:
+			self.MPLAY_process.kill()		
+		except:
+			print 'no process to kill'		
 
 	def threadFunction(self):
 		self.thread = threading.Thread(target=self.launchMplay)
@@ -181,9 +185,9 @@ class myClass(QtGui.QMainWindow):
 
 		self.CANCELED = False
 		bBreak = False
-		path = "F:/BLENDER_playground/render/world/*.png"
+
 		# F:\HOUDINI_15_playground\render\fur_sea
-		obj = glob.glob(path)
+		obj = glob.glob(self.SRCPATH)
 		newNum = len(obj)
 
 
@@ -191,33 +195,33 @@ class myClass(QtGui.QMainWindow):
 
 			'''command options *
 				-T : always on top
-
+				-minimal 
 			'''
-			self.MPLAY_process = subprocess.Popen("C:/Program Files/Side Effects Software/Houdini 15.0.313/bin/mplay.exe -minimal -p -z 100 -f 1 "+str(newNum)+" 1 -r 25 F:/BLENDER_playground/render/world/*.png",
+			self.MPLAY_process = subprocess.Popen("C:/Program Files/Side Effects Software/Houdini 15.0.313/bin/mplay.exe -minimal  -p -z 200 -f 1 "+str(newNum)+" 1 -r 25 "+self.SRCPATH,
 										shell=False,
 										stdin=subprocess.PIPE,
 										stdout=subprocess.PIPE) 
 
 
 
-			obj  = glob.glob(path)
+			obj  = glob.glob(self.SRCPATH)
 			num = len(obj)
-			oldNum = num
+			oldNum = num-1
 			
-
+			bBreak = self.CANCELED
 			while not bBreak :
 
 				bBreak = self.CANCELED
-				newNum = len(glob.glob(path))
+				newNum = len(glob.glob(self.SRCPATH))-1
 				print newNum
 				if newNum > oldNum :
 					print "new file detected"
 					bBreak = True
 
 				else:
-					n = 0.05
-					print ("sleep %s minutes" % (n))
-					print ('break -->', bBreak)
+					n = 0.01
+					#print ("sleep %s minutes" % (n))
+					#print ('break -->', bBreak)
 					# print process.stdin
 					time.sleep(60*n)
 
@@ -233,6 +237,8 @@ class myClass(QtGui.QMainWindow):
 					break
 			except:
 				print "no process to kill -- continuing"
+
+			
 
 
 if __name__ == '__main__':
